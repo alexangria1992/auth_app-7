2f06232bb571d432bba7f01d0bf6262e3ea14882
Alexandre Angrignon <alex040892@gmail.com>
2024-04-13 19:38:42 +1200

    Successful Registration on Frontend

---
+++ frontend/src/Register.jsx
@@ -0,0 +1,99 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const Register = () => {
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confPassword: "",
+  });
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios
+      .post("http://localhost:3001/register", values)
+      .then((res) => {
+        if (res.data.Status === "Successful Registration") {
+          navigate("/login");
+        } else {
+          alert("Error");
+        }
+      })
+      .then((err) => console.log(err));
+  };
+  return (
+    <div className="d-flex justify-content-center align-items-center bg-primary vh-100">
+      <div className="bg-white p-3 rounded w-25">
+        <h2>Sign Up</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="name">
+              <strong>Name</strong>
+            </label>
+            <input
+              type="text"
+              placeholder="Enter Name"
+              name="name"
+              className="form-control rounded-0"
+              onChange={(e) => setValues({ ...values, name: e.target.value })}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="email">
+              <strong>Email</strong>
+            </label>
+            <input
+              type="email"
+              placeholder="Enter Email"
+              name="email"
+              className="form-control rounded-0"
+              onChange={(e) => setValues({ ...values, email: e.target.value })}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password">
+              <strong>Password</strong>
+            </label>
+            <input
+              type="password"
+              placeholder="Enter Password"
+              name="password"
+              className="form-control rounded-0"
+              onChange={(e) =>
+                setValues({ ...values, password: e.target.value })
+              }
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="confirm password">
+              <strong>Confirm Password</strong>
+            </label>
+            <input
+              type="password"
+              placeholder="Confirm Password"
+              name="confPassword"
+              className="form-control rounded-0"
+              onChange={(e) =>
+                setValues({ ...values, confPassword: e.target.value })
+              }
+            />
+          </div>
+          <button className="btn btn-success w-100 rounded-0">Sign Up</button>
+          <p>You are agree to our terms and policies</p>
+          <Link
+            to="/login"
+            className="btn btn-default border w-100 bg-light rounded-0 text-decoration-none"
+          >
+            Login
+          </Link>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
