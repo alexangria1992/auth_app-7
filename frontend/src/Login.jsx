3c6f4fc64e0a0af45ad48f95dd69ede5ceb8705a
Alexandre Angrignon <alex040892@gmail.com>
2024-04-13 19:48:50 +1200

    Successful Login on Frontend

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,5 +1,72 @@
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+
 const Login = () => {
-  return <div>Login</div>;
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    axios
+      .post("http://localhost:3001/login", values)
+      .then((res) => {
+        if (res.data.Status === "Success") {
+          navigate("/");
+        } else {
+          alert("Error");
+        }
+      })
+      .then((err) => console.log(err));
+  };
+  return (
+    <div className="d-flex justify-content-center align-items-center bg-primary vh-100">
+      <div className="bg-white p-3 rounded w-25">
+        <h2>Sign In</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-3">
+            <label htmlFor="email">
+              <strong>Email</strong>
+            </label>
+            <input
+              type="email"
+              placeholder="Enter Email"
+              name="email"
+              className="form-control rounded-0"
+              onChange={(e) => setValues({ ...values, email: e.target.value })}
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="password">
+              <strong>Password</strong>
+            </label>
+            <input
+              type="password"
+              placeholder="Enter Password"
+              name="password"
+              className="form-control rounded-0"
+              onChange={(e) =>
+                setValues({ ...values, password: e.target.value })
+              }
+            />
+          </div>
+
+          <button className="btn btn-success w-100 rounded-0">Log In</button>
+          <p>You are agree to our terms and policies</p>
+          <Link
+            to="/register"
+            className="btn btn-default border w-100 bg-light rounded-0 text-decoration-none"
+          >
+            Create Account
+          </Link>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
